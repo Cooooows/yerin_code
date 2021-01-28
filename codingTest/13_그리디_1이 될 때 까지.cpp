d3d90a86d2ab7d2b7f8e33d248b52e8599ccb824
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-01-28 21:54:58 +0900

    Update 13_그리디_1이 될 때 까지.cpp

--- codingTest/13_그리디_1이 될 때 까지.cpp
+++ codingTest/13_그리디_1이 될 때 까지.cpp
@@ -1,3 +1,6 @@
+//입력값 : N(1이 되어야하는 수) K(나누는 수)
+//문제 : 1. N이 K로 나누어지지 않으면 N에서 1을 뺸다. 2. N이 K로 나누어지면 나눈다. 1번, 2번을 N이 1이 될때까지 반복
+//출력값 : N이 1이 될때까지 action을 행한 횟수
 #include <iostream>
 
 using namespace std;
@@ -14,8 +17,8 @@ int main()
 	{
 		if (N % K != 0)
 		{
-			int mod = N % K;
-			N = N - mod;
+			int mod = N % K;	//나누고 남은수 == 앞으로 1을 뺄 수
+			N = N - mod;		//나누어 지는 수가 되도록 뺴준다.
 			res = res + mod;
 		}
 		else
