403b2fdb6bc8995dc1ede5ddbc516ddf3d28f68a
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-01-28 15:53:12 +0900

    Update 13_그리디_1이 될 때 까지.cpp

--- codingTest/13_그리디_1이 될 때 까지.cpp
+++ codingTest/13_그리디_1이 될 때 까지.cpp
@@ -2,7 +2,54 @@
 
 using namespace std;
 
+
 int main()
 {
-	
+	int N, K;
+	int res = 0;
+
+	cin >> N >> K;
+
+	while (N != 1)
+	{
+		if (N % K != 0)
+		{
+			int mod = N % K;
+			N = N - mod;
+			res = res + mod;
+		}
+		else
+		{
+			N = N / K;
+			res++;
+		}
+	}
+
+	cout << res << endl;
 }
+
+
+/*
+int main()
+{
+	int N, K;
+	int res = 0;
+
+	cin >> N >> K;
+
+	while (N != 1)
+	{
+		if (N % K == 0)
+		{
+			N = N / K;
+			res++;
+			continue;
+		}
+		N = N - 1;
+		res++;
+	}
+
+	cout << res << endl;
+}
+
+*/
