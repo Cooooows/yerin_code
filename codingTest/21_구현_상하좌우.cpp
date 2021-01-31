c66f08e437644ac30bbb063be44264d598286235
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-01-31 21:39:43 +0900

    Update 21_구현_상하좌우.cpp

--- codingTest/21_구현_상하좌우.cpp
+++ codingTest/21_구현_상하좌우.cpp
@@ -1,20 +1,33 @@
+/*
+	//map을 사용해서 해보자
+	map<char, vector<int>> dir;
+
+	dir['U'] = vector<int>{ -1,0 };
+	dir['D'] = vector<int>{ 1,0 };
+	dir['L'] = vector<int>{ 0,-1};
+	dir['R'] = vector<int>{ 0,1 };
+*/
+
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+
 using namespace std;
+
 
 int main()
 {
 	int n = 0;
 	int x = 1, y = 1;
 	string course;
-	char dir = 0;
 
 	cin >> n;
 
 	cin.ignore();
 
 	getline(cin, course);
-	
+
 	for (int i = 0; i < course.length(); i = i+2)
 	{
 		if (course[i] == 'L')
@@ -54,8 +67,8 @@ int main()
 			cout << "error" << endl;
 			break;
 		}
-		
+
 	}
-	
+
 	cout << x << " " << y << endl;
 }
