4bc2d7d53f5c2674ddf73b0ca107928de09bfac6
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-02-18 00:35:50 +0900

    Update 62_다이나믹_개미전사_탈락.cpp

--- codingTest/62_다이나믹_개미전사_탈락.cpp
+++ codingTest/62_다이나믹_개미전사_탈락.cpp
@@ -1,5 +1,6 @@
 //8
-//1 2 3 3 1 1 1 2 
+//1 1 3 3 2 1 1 2 
+//8
 
 #include <iostream>
 #include <vector>
@@ -9,11 +10,11 @@ using namespace std;
 int main()
 {
 	int n = 0;
-	vector<pair<int, int>> storage;
+	vector<pair<int, int>> storage;	//창고
 
 	cin >> n;
 
-	int* arr = new int[n]();
+	int* arr = new int[n]();	//창고를 털었는지 확인해주는 친구
 
 	int input;
 	for (int i = 0; i < n; i++)
