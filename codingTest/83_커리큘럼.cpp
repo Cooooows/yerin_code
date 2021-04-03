90f55a447dbfc03bce489644a929916ea27ef5b4
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-04-03 14:21:39 +0900

    Update 83_커리큘럼.cpp

--- codingTest/83_커리큘럼.cpp
+++ codingTest/83_커리큘럼.cpp
@@ -25,12 +25,14 @@ vector<int> graph[100001];
 // 위상 정렬 함수
 void topologySort()
 {
-    vector<int> result; // 알고리즘 수행 결과를 담을 리스트
+    vector<int> result(501); // 알고리즘 수행 결과를 담을 리스트
     queue<int> q;
+
 
     // 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
     for (int i = 1; i <= n; i++)
     {
+        result[i] = classtime[i];
         if (indegree[i] == 0) //진입 차수가 0이면 큐에 삽입
         {
             q.push(i);  //노드 번호를 push
@@ -42,13 +44,10 @@ void topologySort()
         // 큐에서 원소 꺼내기
         int now = q.front();
         q.pop();
-        //result[now] = classtime[now];
-        printf("???");
-        // 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+
         for (int i = 0; i < graph[now].size(); i++)
         {
-            result[i] = max(result[i], result[now] + classtime[i]);
-            printf("result : %d, i : %d", result[i], i);
+            result[graph[now][i]] = max(result[graph[now][i]], result[now] + classtime[graph[now][i]]);
             indegree[graph[now][i]] -= 1;
             // 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
             if (indegree[graph[now][i]] == 0)
@@ -59,9 +58,9 @@ void topologySort()
     }
 
     // 위상 정렬을 수행한 결과 출력
-    for (int i = 0; i < result.size(); i++)
+    for (int i = 0; i < n+1; i++)
     {
-        cout << result[i] << ' ';
+        cout << result[i] << '\n';
     }
 }
 
